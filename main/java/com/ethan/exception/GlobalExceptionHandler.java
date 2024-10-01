e1f0154e840b1c7615ef08b9d1fd5de0f27d838a
Ethan <2553035342@qq.com>
2024-10-02 00:25:44 +0800

    删除多余冒号

--- main/java/com/ethan/exception/GlobalExceptionHandler.java
+++ main/java/com/ethan/exception/GlobalExceptionHandler.java
@@ -2,7 +2,7 @@ package com.ethan.exception;
 
 import com.ethan.model.result.Result;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
